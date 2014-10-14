06557cf73d83a9438dbe7a08ad7885cca9255605
Bach Van Tuan <bachvtuan@gmail.com>
2014-10-14 10:20:36 +0700

    tweak

--- code/app/public/frontend/js/general.js
+++ code/app/public/frontend/js/general.js
@@ -120,3 +120,46 @@ function roundNumber(number,decimal_points) {
     return v.toString(16);
   });
 };
+
+alertify.defaults = {
+        // dialogs defaults
+        modal:true,
+        movable:true,
+        resizable:true,
+        closable:true,
+        maximizable:true,
+        pinnable:true,
+        pinned:true,
+        padding: true,
+        overflow:true,
+        maintainFocus:true,
+        transition:'pulse',
+
+        // notifier defaults
+        notifier:{
+            // auto-dismiss wait time (in seconds)  
+            delay:5,
+            // default position
+            position:'bottom-right'
+        },
+
+        // language resources 
+        glossary:{
+            // dialogs default title
+            title:'Music app',
+            // ok button text
+            ok: 'OK',
+            // cancel button text
+            cancel: 'Cancel'            
+        },
+
+        // theme settings
+        theme:{
+            // class name attached to prompt dialog input textbox.
+            input:'ajs-input',
+            // class name attached to ok button
+            ok:'ajs-ok',
+            // class name attached to cancel button 
+            cancel:'ajs-cancel'
+        }
+    };
